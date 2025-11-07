8627244f881a65047caa714f2191cf9cbe015e9f
Guillermo Bermejo <guillebermejo1@gmail.com>
2025-11-07 11:28:31 -0300

    feat(admin): mostrar miniatura en lista de artículos

--- resources/views/admin/articles/index.blade.php
+++ resources/views/admin/articles/index.blade.php
@@ -121,6 +121,19 @@
                                         <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
                                     </svg>
                                 @endif
+                                
+                                {{-- Miniatura de la imagen destacada --}}
+                                @php $thumb = $article->getFeaturedImageUrl('thumb') ?? $article->getFeaturedImageUrl(); @endphp
+                                <div class="flex-shrink-0 mr-3">
+                                    @if($thumb)
+                                        <img src="{{ $thumb }}" alt="{{ $article->title }}" class="h-12 w-16 object-cover rounded-md border">
+                                    @else
+                                        <div class="h-12 w-16 bg-gray-100 rounded-md flex items-center justify-center text-xs text-gray-400 border">
+                                            Sin imagen
+                                        </div>
+                                    @endif
+                                </div>
+
                                 <div>
                                     <div class="text-sm font-medium text-gray-900">
                                         {{ Str::limit($article->title, 60) }}
